301a54049a475caa3f3ac5c2318dc494fdf5b04b
RahulBayad <rahulbayad05@gmail.com>
2025-05-26 23:18:54 +0530

    Mysql DB initialization

---
+++ src/db/database.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2";
+
+const dbConnection = async () => {
+  try {
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOSTNAME || "localhost",
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASSWORD || "Rahul@123",
+      database: process.env.DB_DATABASE || "sys",
+    });
+  } catch (error) {
+    console.log("Error while connection to database,", error);
+  }
+};
+
+export default dbConnection;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import { configDotenv } from "dotenv"
-import { app } from "./app"
+import { config } from "dotenv"
+import { app } from "./app.js"
+import dbConnection from "./db/database.js"
 
-configDotenv({
-    path : "./.env"
+config({
+    path : "./src/.env"
 })
 
+dbConnection().then(()=>{
+    console.log("db connected");
+})
 app.listen( process.env.PORT || 3002 , ()=>{
-    console.log("Server started at port ", PORT )
+    console.log("Server started at port", process.env.PORT )
 })
