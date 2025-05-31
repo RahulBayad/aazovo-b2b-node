1b024361a343e8e27ff52f141bb61d77f4a430de
RahulBayad <rahulbayad05@gmail.com>
2025-05-31 10:57:11 +0530

    some change in multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -2,11 +2,11 @@ import multer, { diskStorage } from "multer";
 import path from "path"
 
 const storage = diskStorage({
-    destination : (req, file, cb)=>{
+    destination : (_, _, cb)=>{
         cb(null, path.join( process.cwd(), 'temp/uploads'))
     },
-    filename : (req, file, cb )=>{
-        cb(null, new Date.now() + "_" + file.originalname)
+    filename : (_, file, cb )=>{
+        cb(null, Date.now() + "_" + file.originalname)
     }
 })
 
